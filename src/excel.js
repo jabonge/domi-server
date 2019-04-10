faf3cf5d526ca3b73d8cc5047904a67081a74524
jabonge <yuwoqhd@gmail.com>
2019-04-10 23:17:35 +0900

    hello

--- src/excel.js
+++ src/excel.js
@@ -50,7 +50,9 @@ export const savedb = async () => {
   };
 
   try {
-    const workbook = xlsx.readFile(path.join(__dirname, "/public/sikdan.xlsx"));
+    const workbook = await xlsx.readFile(
+      path.join(__dirname, "/public/sikdan.xlsx")
+    );
     const first_sheet_name = workbook.SheetNames[0];
     const ws = workbook.Sheets[first_sheet_name];
     if (ws["B2"] !== undefined) {
