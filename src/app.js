4c4968d7b5517d08b1623fc688a6c9cd77f4b56a
jabonge <yuwoqhd@gmail.com>
2019-04-12 20:36:57 +0900

    bangchim

--- src/app.js
+++ src/app.js
@@ -5,8 +5,14 @@ import { Api } from "./api";
 
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
 app.use(helmet());
 app.use(process.env.PRODUCTION ? morgan("combined") : morgan("dev"));
 app.get("/", Api);
+app.get("/authority", (req, res) => {
+  res.render("jaebongbangchim.html");
+});
 
 export default app;
